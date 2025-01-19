e8887e43f2119f4e6c9bd2d40cdea4c219fdcf4d
Sai2963 <venkatsai0629@gmail.com>
2025-01-19 19:29:17 +0530

    Products api added

--- app.js
+++ app.js
@@ -48,6 +48,86 @@ app.get("/api/get", async (req, res) => {
   const posts = await prisma.bags.findMany();
   res.json(posts);
 });
+app.get("/api/products/get",async (req,res)=>{
+  try {
+    const response3 = axios.get("https://vipbags.com/collections/new-arrivals/products.json");
+    const response4 = axios.get("https://vipbags.com/collections/bestsellers/products.json");
+    const response5 = axios.get("https://vipbags.com/collections/hard-luggage/products.json");
+    const response6 = axios.get("https://vipbags.com/collections/soft-luggage/products.json");
+    const response7 = axios.get("https://vipbags.com/collections/cabin-luggage/products.json");
+    const response8 = axios.get("https://vipbags.com/collections/check-in-luggage/products.json");
+    const response9 = axios.get("https://vipbags.com/collections/lightweight-luggage/products.json");
+    const response10 = axios.get("https://vipbags.com/collections/luggage-set/products.json");
+    const response11 = axios.get("https://vipbags.com/collections/duffles/products.json");
+    const response12 = axios.get("https://skybags.co.in/collections/hard-luggage/products.json");
+    const response13 = axios.get("https://skybags.co.in/collections/soft-luggage/products.json");
+    const response14 = axios.get("https://skybags.co.in/collections/cabin-luggage/products.json");
+    const response15 = axios.get("https://skybags.co.in/collections/medium-luggage/products.json");
+    const response16 = axios.get("https://skybags.co.in/collections/large-luggage/products.json");
+    const response17 = axios.get("https://skybags.co.in/collections/tsa-lock-luggage/products.json");
+    const response18 = axios.get("https://skybags.co.in/collections/anti-theft-luggage/products.json");
+    const response19 = axios.get("https://skybags.co.in/collections/dual-wheels-luggage/products.json");
+    const response20 = axios.get("https://skybags.co.in/collections/backpacks/products.json");
+    const response21 = axios.get("https://skybags.co.in/collections/duffle/products.json");
+    const response22 = axios.get("https://skybags.co.in/collections/travelling-bags-for-students/products.json");
+    const response23 = axios.get("https://safaribags.com/collections/trolley-bags/products.json");
+    const response24 = axios.get("https://safaribags.com/collections/backpacks/products.json");
+    const response25 = axios.get("https://safaribags.com/collections/duffles/products.json");
+    const response26 = axios.get("https://safaribags.com/collections/accessories/products.json");
+    const response27 = axios.get("https://www.capresebags.com/collections/all/products.json");
+    const response28 = axios.get("https://www.capresebags.com/collections/handbags-for-women/products.json");
+    const response29 = axios.get("https://www.capresebags.com/collections/slings-for-women/products.json");
+    const response30 = axios.get("https://www.capresebags.com/collections/accessories-for-women/products.json");
+    const response31 = axios.get("https://www.capresebags.com/collections/backpacks-for-women/products.json");
+
+    
+
+    const responses = await Promise.all([
+      response3,
+      response4,
+      response5,
+      response6,
+      response7,
+      response8,
+      response9,
+      response10,
+      response11,
+      response12,
+      response13,
+      response14,
+      response15,
+      response16,
+      response17,
+      response18,
+      response19,
+      response20,
+      response21,
+      response22,
+      response23,
+      response24,
+      response25,
+      response26,
+      response27,
+      response28,
+      response28,
+      response29,
+      response30
+
+
+    ]);
+
+    const combinedData = responses.reduce((acc, response) => {
+      return acc.concat(response.data.products);
+    }, []);
+    const response=combinedData;
+    res.json(response);
+  }
+  catch (error) {
+    console.error("Error:", error.message);
+    setError(error.message);
+    setLoading(false);
+  }
+})
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
