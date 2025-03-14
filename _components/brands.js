c42e894dc675033868367996854ff771075d3ce2
Sai2963 <venkatsai0629@gmail.com>
2025-03-14 16:31:06 +0530

    Multiple Images and Videos

--- _components/brands.js
+++ _components/brands.js
@@ -1,31 +1,63 @@
 function BrandDetails() {
-    const Details = {
-        VIP : {
-            name : "VIP",
-            imageUrl:"https://vipbags.com/cdn/shop/files/aer_height_600.png?v=1726217388",
-            videoUrl :"https://cdn.shopify.com/videos/c/o/v/872a4fccb7624b9ab4312d7275aff538.mp4",
+  const Details = {
+    VIP: {
+      name: "VIP",
+      imageUrl: [
+        "https://vipbags.com/cdn/shop/files/aer_height_600.png?v=1726217388",
+        "https://vipbags.com/cdn/shop/files/vapour_height_600.png?v=1726217388",
+        "https://vipbags.com/cdn/shop/files/height600_2.png?v=1726217388",
+      ],
+      videoUrl: [
+        "https://cdn.shopify.com/videos/c/o/v/872a4fccb7624b9ab4312d7275aff538.mp4",
+        "https://cdn.shopify.com/videos/c/o/v/440e93b80aa449ac9dc70625a2f27fe4.mp4",
+        "https://cdn.shopify.com/videos/c/o/v/1396ca40a00f432884269abb3d43a006.mp4",
+      ],
+    },
+    Skybags: {
+      name: "Skybags",
+      imageUrl: [
+        "https://skybags.co.in/cdn/shop/files/New_Desktop_Banner_1920x900_1_87_2048x.jpg?v=1714386326",
+        "https://skybags.co.in/cdn/shop/files/Student_Travel_1_2048x.png?v=1695297287",
+        "https://skybags.co.in/cdn/shop/files/StayCaution_2048x.png?v=1695273639",
+        "https://skybags.co.in/cdn/shop/files/Adventure_2770a480-66b7-4eff-aa4f-cffc05f20477_2048x.png?v=1710741918",
+      ],
 
-        },
-        Skybags : {
-            name : "Skybags",
-            imageUrl:"https://skybags.co.in/cdn/shop/files/New_Desktop_Banner_1920x900_1_87_2048x.jpg?v=1714386326",
-            videoUrl :"https://cdn.shopify.com/videos/c/o/v/065fd55870c646719c5ee8d53b5d71c2.mp4",
-        },
-        Safari : {
-            name : "Safari",
-            imageUrl : "https://images.freekaamaal.com/post_images/1690540255.webp",
-            videoUrl : "https://safaribags.com/cdn/shop/videos/c/vp/918803e391bc476ca8d09a2ce2d2e6fa/918803e391bc476ca8d09a2ce2d2e6fa.HD-1080p-2.5Mbps-42794998.mp4?v=0",
+      videoUrl: [
+        "https://cdn.shopify.com/videos/c/o/v/065fd55870c646719c5ee8d53b5d71c2.mp4",
+        "https://cdn.shopify.com/videos/c/o/v/065fd55870c646719c5ee8d53b5d71c2.mp4",
+        "https://cdn.shopify.com/videos/c/o/v/47516ab363ff4e4e9b538e1f677b164b.mp4",
+        "https://cdn.shopify.com/videos/c/o/v/b757fe2c4fc6418483b0f87a0e4228d9.mp4",
+      ],
+    },
+    Safari: {
+      name: "Safari",
+      imageUrl: [
+        "https://images.freekaamaal.com/post_images/1690540255.webp",
+        "https://safaribags.com/cdn/shop/files/Archit_1_800x.jpg?v=1686298576",
+        // "https://safaribags.com/cdn/shop/files/SF-LuggageCollection-Banner-2160_x_1080.jpg?v=1726637548&width=1000",
+        // "https://th.bing.com/th/id/OIP.AoUWKNekBn9mC156_yglTQHaDt?rs=1&pid=ImgDetMain",
+      ],
 
-        },
-        Caprese : {
-            name : "Caprese",
-            imageUrl : "https://th.bing.com/th/id/OIP.vJcALo1Go1ydODh5ZsMSTQHaBu?w=273&h=180&c=7&r=0&o=5&dpr=1.6&pid=1.7",
-            videoUrl : "https://cdn.shopify.com/videos/c/o/v/c11c6088a33245d5bed9b5bd705dacc4.mp4",
-        }
+      videoUrl: [
+        "https://safaribags.com/cdn/shop/videos/c/vp/918803e391bc476ca8d09a2ce2d2e6fa/918803e391bc476ca8d09a2ce2d2e6fa.HD-1080p-2.5Mbps-42794998.mp4?v=0",
+        "https://safaribags.com/cdn/shop/videos/c/vp/747872c2d038403995fe428e2998f9cf/747872c2d038403995fe428e2998f9cf.HD-1080p-7.2Mbps-30723148.mp4?v=0",
+      ],
+    },
+    Caprese: {
+      name: "Caprese",
+      imageUrl: [
+        "https://www.hellonaari.com/wp-content/uploads/2023/04/Caprese.png",
+        "https://www.capresebags.com/cdn/shop/files/Aurora_Homepage-Banner_Desktop-with-CTA.jpg?v=1700817797&width=1200",
+        "https://th.bing.com/th/id/OIP.HtVS7l2KLYvLURwfWGZ4FQHaFj?w=252&h=189&c=7&r=0&o=5&dpr=1.6&pid=1.7",
+      ],
 
-            
-            
-    }
-    return Details;
+      videoUrl: [
+        "https://cdn.shopify.com/videos/c/o/v/c11c6088a33245d5bed9b5bd705dacc4.mp4",
+        "https://www.capresebags.com/cdn/shop/files/quinn_yrukde0wr6qyvd8jk5n45v2f.mp4",
+        "https://www.capresebags.com/cdn/shop/files/quinn_ku4cy2qcpvp32t27pei1taut.mp4"
+      ],
+    },
+  };
+  return Details;
 }
 module.exports = BrandDetails;
